8f523064d367c6320d25b1331387fb68a7b8e599
David-Pogner <baumgartingerdavid@gmail.com>
2024-05-06 21:04:50 +0200

    erste Design-Arbeiten an der HomePage + hinzufügen von homePageImage im img folder

--- frontend/dashboard/userSettings/app.js
+++ frontend/dashboard/userSettings/app.js
@@ -4,14 +4,6 @@ function init() {
     if (!user) {
         window.location.href = "../../signIn";
     }
-
-    const logOut = document.getElementById("logOut");
-    logOut.addEventListener("click", () => {
-        localStorage.removeItem("user");
-        window.location.reload();
-    });
-
 }
-
 
 document.addEventListener("DOMContentLoaded", init);
