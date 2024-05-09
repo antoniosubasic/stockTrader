905299e3f4ef28f1d1e7c9a40b672c1dec4ce230
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-05-09 11:39:34 +0200

    Adding first beta version of the markets

--- frontend/markets/app.js
+++ frontend/markets/app.js
@@ -1,41 +1,82 @@
 import endpoint from "../assets/scripts/config.js";
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch(`${endpoint}/market?symbol=AAPL`);
+async function init() {
+    const searchForm = document.getElementById("search-form");
 
-    if (response.ok) {
-        const market = await response.json();
-        const marketName = document.getElementById('market-name');
-        marketName.innerHTML = market.symbol;
+    searchForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const symbol = searchForm.search.value;
 
-        const stockPrices = market.stockPrices;
-        const stockPricesTable = document.getElementById('stock-prices-table');
+        const marketDiv = document.getElementById("chart");
+        const drawer = new Drawer(symbol, marketDiv);
 
-        for (const stockPrice of stockPrices) {
-            const tr = document.createElement('tr');
-            const timestamp = document.createElement('td');
-            timestamp.innerHTML = new Date(stockPrice.timestamp).toLocaleDateString();
-            tr.appendChild(timestamp);
-
-            const open = document.createElement('td');
-            open.innerHTML = stockPrice.open;
-            tr.appendChild(open);
-
-            const high = document.createElement('td');
-            high.innerHTML = stockPrice.high;
-            tr.appendChild(high);
-
-            const low = document.createElement('td');
-            low.innerHTML = stockPrice.low;
-            tr.appendChild(low);
-
-            const close = document.createElement('td');
-            close.innerHTML = stockPrice.close;
-            tr.appendChild(close);
-
-            stockPricesTable.appendChild(tr);
+        if (await drawer.getMarket()) {
+            drawer.drawMarket(30);
+        } else {
+            console.error("failed to get market data");
         }
-    } else {
-        console.error(await response.text());
+    });
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+class Drawer {
+    constructor(symbol, marketDiv) {
+        this.symbol = symbol;
+        this.marketDiv = marketDiv;
+        this.market;
+        this.stockPrices;
+
     }
-});
+
+    async getMarket() {
+        const response = await fetch(
+            `${endpoint}/market?symbol=${this.symbol}`
+        );
+
+        if (response.ok) {
+            this.market = await response.json();
+            this.stockPrices = this.market.stockPrices;
+            return true;
+        } else {
+            console.error(await response.text());
+            return false;
+        }
+    }
+
+    drawMarket(days) {
+        while (this.marketDiv.firstChild) {
+            this.marketDiv.removeChild(this.marketDiv.firstChild);
+        }
+        const newCanvas = document.createElement("canvas");
+        this.marketDiv.appendChild(newCanvas);
+
+        const ctx = newCanvas.getContext("2d");
+
+        const labels = this.stockPrices
+            .map((stockPrice) =>
+                new Date(stockPrice.timestamp).toLocaleDateString()
+            )
+            .slice(-days);
+
+        const data = this.stockPrices
+            .map((stockPrice) => stockPrice.close)
+            .slice(-days);
+
+        new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: this.market.name,
+                        data: data,
+                        fill: false,
+                        borderColor: "rgb(75, 192, 192)",
+                        tension: 0.1,
+                    },
+                ],
+            },
+        });
+    }
+}
