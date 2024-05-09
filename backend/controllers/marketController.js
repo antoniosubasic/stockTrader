4c2d3a47839c16e1fb9be2bd301c0b8b93d1b7ef
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-05-09 08:49:37 +0200

    updated markets on backend updated dynamic fetching - fetch only done if needed added UTC timestamp saving of last fetch added error handling for cooldown request of polygon API

--- backend/controllers/marketController.js
+++ backend/controllers/marketController.js
@@ -4,33 +4,37 @@ const marketsFile = './data/markets.json';
 const apiKeyFile = './data/apikey';
 const stockPricesFolder = './data/stockPrices';
 
-const dateToISO = date => date.toISOString().split('T')[0];
+const timestampToISO = timestamp => new Date(timestamp).toISOString().split('T')[0];
+const UTCTimestamp = (daysAgo = null) => {
+    const now = new Date();
+    const ago = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - (daysAgo || 0));
+    return Date.UTC(ago.getUTCFullYear(), ago.getUTCMonth(), ago.getUTCDate());
+}
 
 export class Controller {
     _markets = null;
     _apiKey = null;
 
     constructor() {
-        this._markets = JSON.parse(fs.readFileSync(marketsFile));
         this._apiKey = fs.readFileSync(apiKeyFile);
+        this._markets = JSON.parse(fs.readFileSync(marketsFile));
 
         this._markets.forEach(market => {
-            const stockPriceFile = `${stockPricesFolder}/${market.symbol}.json`;
-            if (fs.existsSync(stockPriceFile)) {
-                market.stockPrices = JSON.parse(fs.readFileSync(stockPriceFile));
+            const stockPricesFile = `${stockPricesFolder}/${market.symbol}.json`;
+            if (fs.existsSync(stockPricesFile)) {
+                market.stockPrices = JSON.parse(fs.readFileSync(stockPricesFile));
             }
         });
     }
 
     save() {
-        var stockPrices = this._markets.map(market => ({
-            symbol: market.symbol,
-            stockPrices: market.stockPrices
-        }));
+        const markets = this._markets.map(({ stockPrices, ...market }) => market);
+        const stockPrices = this._markets.filter(market => market.stockPrices).map(({ symbol, stockPrices }) => ({ symbol, stockPrices }));
 
-        stockPrices.filter(stockPrice => stockPrice.stockPrices).forEach(stockPrice => {
-            const stockPriceFile = `${stockPricesFolder}/${stockPrice.symbol}.json`;
-            fs.writeFileSync(stockPriceFile, JSON.stringify(stockPrice.stockPrices));
+        fs.writeFileSync(marketsFile, JSON.stringify(markets));
+        stockPrices.forEach(s => {
+            const stockPricesFile = `${stockPricesFolder}/${s.symbol}.json`;
+            fs.writeFileSync(stockPricesFile, JSON.stringify(s.stockPrices));
         });
     }
 
@@ -38,21 +42,28 @@ export class Controller {
         return this._markets.find(market => market.symbol === symbol);
     }
 
-    async fetchStockPrices(symbol, daysAgo) {
-        const end = new Date();
-        end.setDate(end.getDate() + 1);
+    async fetchStockPrices(symbol) {
+        const market = this.get(symbol);
 
-        const start = new Date();
-        start.setDate(end.getDate() - daysAgo);
+        const latestFetch = market.latestFetch;
+        const now = UTCTimestamp();
 
-        const response = await fetch(`https://api.polygon.io/v2/aggs/ticker/${symbol}/range/1/day/${dateToISO(start)}/${dateToISO(end)}/?adjusted=true&sort=asc&limit=50000&apiKey=${this._apiKey}`);
-        const data = await response.json();
+        if (!latestFetch || latestFetch < now) {
+            const response = await fetch(`https://api.polygon.io/v2/aggs/ticker/${symbol}/range/1/day/${timestampToISO(latestFetch || UTCTimestamp(365))}/${timestampToISO(now)}/?adjusted=true&sort=asc&limit=50000&apiKey=${this._apiKey}`);
 
-        if (data.results) {
-            const stockPrices = data.results.map(({ o: open, c: close, h: high, l: low, t: timestamp }) => ({ open, close, high, low, timestamp }));
-            this.get(symbol).stockPrices = stockPrices;
-            this.save();
-        }
+            if (response.ok) {
+                const data = await response.json();
+                const stockPrices = data.results.map(({ o: open, c: close, h: high, l: low, t: timestamp }) => ({ open, close, high, low, timestamp }));
+
+                if (!market.stockPrices) { market.stockPrices = stockPrices; }
+                else { market.stockPrices.push(...stockPrices); }
+
+                market.latestFetch = now;
+                this.save();
+
+                return [200, 'stock prices fetched'];
+            } else { return [response.status, response.statusText]; }
+        } else { return [304, 'stock prices up to date']; }
     }
 
     get markets() {
