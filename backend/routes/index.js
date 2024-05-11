31cc432d9b88f1cfcf12a54cadebadf5daff920d
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-05-11 23:07:46 +0200

    Added stockGainers and stockLoseres

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { User } from '../models/userModel.js';
-import { Market, Markets } from '../models/marketModel.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { User } from "../models/userModel.js";
+import { Market, Markets } from "../models/marketModel.js";
 
 const port = 8000;
 const app = express();
@@ -10,39 +10,63 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/user/signin', (req, res) => {
+app.post("/user/signin", (req, res) => {
     const { username, password } = req.body;
     const [user, [status, message]] = User.signIn(username, password);
-    return user ? res.status(status).json(user) : res.status(status).send(message);
+    return user
+        ? res.status(status).json(user)
+        : res.status(status).send(message);
 });
 
-app.post('/user/signup', (req, res) => {
+app.post("/user/signup", (req, res) => {
     const { username, password } = req.body;
     const [status, message] = User.signUp(username, password);
     return res.status(status).send(message);
 });
 
-app.post('/user/delete', (req, res) => {
+app.post("/user/delete", (req, res) => {
     const { username, password } = req.body;
     const [status, message] = User.delete(username, password);
     return res.status(status).send(message);
 });
 
-app.get('/market', async (req, res) => {
+app.get("/market", async (req, res) => {
     const { symbol } = req.query;
 
     if (!symbol) {
-        return res.status(400).send('missing symbol query parameter');
+        return res.status(400).send("missing symbol query parameter");
     }
 
     const market = new Market(symbol);
     const [data, [status, message]] = await market.get();
 
-    return status === 200 ? res.status(status).json(data) : res.status(status).send(message);
+    return status === 200
+        ? res.status(status).json(data)
+        : res.status(status).send(message);
 });
 
-app.get('/markets', (_, res) => {
+app.get("/markets", (_, res) => {
     return res.status(200).json(Markets.getAll());
+});
+
+app.get("/markets/gainers", (req, res) => {
+    const { count } = req.query;
+
+    if (!count) {
+        return res.status(400).send("missing count query parameter");
+    }
+
+    return res.status(200).json(Markets.getTopGainers(count));
+});
+
+app.get("/markets/losers", (req, res) => {
+    const { count } = req.query;
+
+    if (!count) {
+        return res.status(400).send("missing count query parameter");
+    }
+
+    return res.status(200).json(Markets.getTopLosers(count));
 });
 
 app.listen(port, () => {
