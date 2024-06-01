ad53987419bcc94123443644244b151002c26016
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-06-01 14:19:48 +0200

    added load-handling and error-handling to `signIn` and `signUp` via bootstrap modal

--- frontend/signIn/app.js
+++ frontend/signIn/app.js
@@ -2,35 +2,59 @@ import endpoint from "../assets/scripts/config.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const signInForm = document.getElementById("signin-form");
-    const errorMessage = document.getElementById("error-message");
+    const responseDiv = document.querySelector("#modal .modal-body .content");
+    const modal = new bootstrap.Modal(document.getElementById('modal'));
 
     signInForm.addEventListener("submit", async (e) => {
         e.preventDefault();
-        errorMessage.innerHTML = "";
 
         const username = signInForm.username.value;
         const password = signInForm.password.value;
 
-        const response = await fetch(`${endpoint}/user/signin`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, password }),
-        });
+        if (username && password) {
+            responseDiv.innerHTML = `
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Signing in...</span>
+                </div>
+            `;
+            modal.show();
 
-        if (response.ok) {
-            if (localStorage.getItem("jwt")) {
-                errorMessage.innerHTML = "<p>User already signed in</p>";
+            const response = await fetch(`${endpoint}/user/signin`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (response.ok) {
+                if (localStorage.getItem("jwt")) {
+                    signInForm.password.value = "";
+                    responseDiv.innerHTML = '<p class="error">User already signed in</p>';
+                } else {
+                    const { user, token: jwt } = await response.json();
+                    localStorage.setItem("jwt", jwt);
+                    localStorage.setItem("user", JSON.stringify(user));
+
+                    responseDiv.innerHTML = `
+                        <p class="success">Sign In successful</p>
+                        <p class="redirect-time"></p>
+                    `;
+
+                    const redirectTimeElement = document.querySelector(".modal-body .content .redirect-time");
+                    let redirectTime = 4;
+                    const redirectInterval = setInterval(() => {
+                        redirectTimeElement.innerText = redirectTime === 0 ? 'Redirecting...' : `Redirecting in ${--redirectTime}...`;
+                        if (redirectTime <= 0) {
+                            clearInterval(redirectInterval);
+                            window.location.href = "../dashboard";
+                        }
+                    }, 1000);
+                }
             } else {
-                const { user, token: jwt } = await response.json();
-                localStorage.setItem("jwt", jwt);
-                localStorage.setItem("user", JSON.stringify(user));
-                window.location.href = "../dashboard";
+                signInForm.password.value = "";
+                responseDiv.innerHTML = `<p class="error">${await response.text()}</p>`;
             }
-        } else {
-            errorMessage.innerHTML = `<p>${await response.text()}</p>`;
-            signInForm.password.value = "";
         }
     });
 });
--- frontend/signUp/app.js
+++ frontend/signUp/app.js
@@ -2,35 +2,76 @@ import endpoint from "../assets/scripts/config.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const signUpForm = document.getElementById('signup-form');
-    const errorMessage = document.getElementById('error-message');
+    const responseDiv = document.querySelector("#modal .modal-body .content");
+    const modal = new bootstrap.Modal(document.getElementById('modal'));
 
     signUpForm.addEventListener('submit', async (e) => {
         e.preventDefault();
-        errorMessage.innerHTML = "";
 
         const username = signUpForm.username.value;
         const password = signUpForm.password.value;
         const confirmPassword = signUpForm.confirmPassword.value;
 
-        if (password !== confirmPassword) {
-            errorMessage.innerHTML = '<p>Passwords do not match</p>';
-            signUpForm.confirmPassword.value = signUpForm.password.value = '';
-            return;
-        }
+        if (username && password && confirmPassword) {
+            responseDiv.innerHTML = `
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Signing in...</span>
+                </div>
+            `;
+            modal.show();
 
-        const response = await fetch(`${endpoint}/user/signup`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        });
+            if (password !== confirmPassword) {
+                responseDiv.innerHTML = '<p class="error">Passwords do not match</p>';
+                signUpForm.confirmPassword.value = signUpForm.password.value = '';
+                return;
+            }
 
-        if (response.ok) {
-            window.location.href = '../signIn';
-        } else {
-            errorMessage.innerHTML = `<p>${await response.text()}</p>`;
-            signUpForm.password.value = signUpForm.confirmPassword.value = '';
+            if (password.length < 8) {
+                responseDiv.innerHTML = '<p class="error">Password must be at least 8 characters long</p>';
+                signUpForm.password.value = signUpForm.confirmPassword.value = '';
+                return;
+            }
+
+            const hasUppercase = /[A-Z]/.test(password);
+            const hasLowercase = /[a-z]/.test(password);
+            const hasDigit = /\d/.test(password);
+            const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+            if (!(hasUppercase && hasLowercase && hasDigit && hasSpecial)) {
+                responseDiv.innerHTML = '<p class="error">Password must contain at least one <span class="bold">uppercase letter</span>, one <span class="bold">lowercase letter</span>, one <span class="bold">digit</span>, and one <span class="bold">special character</span></p>';
+                signUpForm.password.value = signUpForm.confirmPassword.value = '';
+                return;
+            }
+
+            const response = await fetch(`${endpoint}/user/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            if (response.ok) {
+                responseDiv.innerHTML = `
+                    <p class="success">Sign Up successful</p>
+                    <p class="redirect-time"></p>
+                `;
+
+                console.log('animate');
+
+                const redirectTimeElement = document.querySelector(".modal-body .content .redirect-time");
+                let redirectTime = 4;
+                const redirectInterval = setInterval(() => {
+                    redirectTimeElement.innerText = redirectTime === 0 ? 'Redirecting...' : `Redirecting in ${--redirectTime}...`;
+                    if (redirectTime <= 0) {
+                        clearInterval(redirectInterval);
+                        window.location.href = "../signIn";
+                    }
+                }, 1000);
+            } else {
+                signUpForm.password.value = signUpForm.confirmPassword.value = '';
+                responseDiv.innerHTML = `<p class="error">${await response.text()}</p>`;
+            }
         }
     });
 });
